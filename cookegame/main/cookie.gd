616ccc694e518bb483cf9951c8f1dfd9798e822b
HannahKnisley <122892305+HannahKnisley@users.noreply.github.com>
2025-03-17 01:02:54 -0400

    pee

--- cookegame/main/cookie.gd
+++ cookegame/main/cookie.gd
@@ -5,7 +5,7 @@ func _ready() -> void:
 	$Camera3D.top_level = true
  
 func _physics_process(delta: float) -> void:
-	$Camera3D.position = Vector3(self.position.x, self.position.y, self.position.z+2)
+	$Camera3D.position = Vector3(self.position.x, self.position.y+0.842, self.position.z+2.318)
 	
 	if Input.is_action_pressed("Forward"):
 		angular_velocity.z -= rolling_force*delta
