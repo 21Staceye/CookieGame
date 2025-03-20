610eed98244141b782f8215a00eece8dcbfb9df1
21Staceye <staceyerin5@gmail.com>
2025-03-20 01:05:53 -0400

    poseMeter

--- cookegame/test_erin/scripts/pose_meter.gd
+++ cookegame/test_erin/scripts/pose_meter.gd
@@ -10,30 +10,59 @@ var poseNum = 0
 var arraySteps = ["BalancePos","StandPos", "TipToesPos"]
 var goal = arraySteps.pick_random()
 func _process(delta):
-	if failed == true and poseNum < 2:
-		print("faileddddddd")
-		$won.visible = false
+	if failed == true and poseNum < 2: # failed game 
+		get_tree().change_scene_to_file("res://main/world_root.tscn")
+	elif passed == true: # passed the game and moves to the world 
+		get_tree().change_scene_to_file("res://main/world_root.tscn")
 	else:
-		$triMov.mesh.size.x = valueSize
+		$happy.visible = false
 		# for the temp poses 
 		if goal == "BalancePos":
-			valueSize = 1.2
+			$cookieSugar2.visible = false
+			$cookieSugar3.visible = false
+			$cookieSugar.visible = false
+			$cookieSugar1.visible = true
 		if goal == "StandPos":
-			valueSize = 0.5
+			$cookieSugar3.visible = false
+			$cookieSugar1.visible = false
+			$cookieSugar.visible = false
+			$cookieSugar2.visible = true
 		if goal == "TipToesPos":
-			valueSize = 0.2
+			$cookieSugar1.visible = false
+			$cookieSugar2.visible = false
+			$cookieSugar.visible = false
+			$cookieSugar3.visible = true
+		if Input.is_action_just_pressed("BalancePos"):
+			$cookie2.visible = false
+			$cookie3.visible = false
+			$cookie.visible = false
+			$cookie1.visible = true
+		if Input.is_action_just_pressed("StandPos"):
+			$cookie1.visible = false
+			$cookie3.visible = false
+			$cookie.visible = false
+			$cookie2.visible = true
+		if Input.is_action_just_pressed("TipToesPos"):
+			$cookie1.visible = false
+			$cookie2.visible = false
+			$cookie.visible = false
+			$cookie3.visible = true
 		if Input.is_action_just_pressed(goal) and poseNum < 3 and stop == false:
+				$happy.visible = true
 				
-				$won.visible = true
-				stop = true
 				poseNum+=1
 				stop = false
 				value = 0
 				$PoseMeterr.value = 0
 				goal = arraySteps.pick_random()
-				$/root/SugarCookieMini/player.mesh.size.x = valueSize
+					
 	if poseNum >= 3:
 		stop = true
+		$happy.visible = true
+		$cookie1.visible = false
+		$cookie2.visible = false
+		$cookie.visible = true
+		$cookie3.visible = false
 		$PoseMeterr.visible = false
 		passed = true
 	# pose metter 
@@ -41,7 +70,7 @@ func _process(delta):
 		if stop == false:
 			# pose metter 
 			$PoseMeterr.value = value
-			value+=0.8
+			value+=0.3
 	if value == 99:
 		stop = true
 		failed = true
@@ -54,4 +83,11 @@ func _on_timer_timeout():
 	timerOut = true
 	stop = true
 	failed = true
+	$PoseMeterr.value = 100
+	$failed.visible = true
+	$cookie1.visible = false
+	$cookie2.visible = false
+	$cookie.visible = true
+	$cookie3.visible = false
+	
 	
