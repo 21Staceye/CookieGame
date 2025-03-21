3d9f501a2e131bbee588530184f47d4d6a42d1f6
HannahKnisley <122892305+HannahKnisley@users.noreply.github.com>
2025-03-21 04:43:30 -0400

    des lines

--- cookegame/main/transitions/transitionScript.gd
+++ cookegame/main/transitions/transitionScript.gd
@@ -10,11 +10,16 @@ var whichGroup = -1 #0 for sugar, 1 for nnn, 2 for chocolate
 
 @onready var UIList = worldScene.get_node("transUI").get_children()
 @onready var camList = worldScene.get_node("camPositions").get_children()
+@onready var introVoiceLines = [worldScene.get_node("VoiceLines/SugarIntro"),worldScene.get_node("VoiceLines/NNNIntro"),worldScene.get_node("VoiceLines/CCIntro")]
+@onready var outroVoiceLines = [worldScene.get_node("VoiceLines/SugarOutro"),worldScene.get_node("VoiceLines/NNNOutro"),worldScene.get_node("VoiceLines/CCOutro")]
+@onready var voicelineTimer = self.get_node("../awaitVoicelineTimer")
+
 var scenesList = ["res://test_erin/Scenes/sugarCookieMini.tscn", 
 					"res://minigames//nnquiz_minigame/quiz_scene.tscn",
 					"res://minigames/wieghtMinigame/Scenes/weightGame.tscn"]
 var dialogues = []	#Modified later to add dialogue text boxes to an array
 var dialogueCounter = 0	 #Modified later to know how many text boxes there are left
+
 
 func cookieInteract(body: Node3D) -> void:
 	if body.name == "cookie":
@@ -36,11 +41,12 @@ func _process(delta) -> void:
 			"chocolate":
 				whichGroup = 1
 		if (player.completes == whichGroup):	#Makes sure we talk to the groups in order
-			UIList[whichGroup].visible = true	#Make corresponding UI visible
 			isTalk = true
 			player.isTalking = true
 			player.freezePos = player.position
-			doDialogue(delta)
+			voicelineTimer.start(introVoiceLines[whichGroup].stream.get_length())
+			introVoiceLines[whichGroup].play()
+			
 		
 	if (Input.is_action_just_pressed("Interact") && canTalk == true):
 		if (dialogueCounter == 0):
@@ -49,7 +55,12 @@ func _process(delta) -> void:
 		dialogueCounter -= 1				#by hiding the one on top of it
 		print(dialogueCounter)
 
-func doDialogue(delta):
+func doDialogue():
 	camMover.transition_camera(player.get_node("Camera3D"), camList[whichGroup], 2) #Does tweening
 	dialogues = UIList[whichGroup].get_children() #Gets the corresponding text box list
 	dialogueCounter = len(dialogues) - 1	#And its length
+
+
+func _on_await_voiceline_timer_timeout() -> void:
+	UIList[whichGroup].visible = true	#Make corresponding UI visible
+	doDialogue()
