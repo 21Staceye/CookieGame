901ed5d70a7354475915097e26eb0c1d627585f5
mageeh192 <haleymagee04@gmail.com>
2025-03-21 01:52:26 -0400

    Comics

---
+++ cookegame/comics/Scenes/comic5Script.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+@onready var panelList = get_node("panels").get_children()
+@onready var num = len(panelList) - 2
+var counter = 0
+
+@onready var changer = get_node("whenToChange")
+
+func _process(delta: float) -> void:
+	panelList[counter].self_modulate.a += 0.5 * delta
+	if (Input.is_action_just_pressed("Interact") && counter == num + 1):
+		get_tree().change_scene_to_file("res://comics/Scenes/comic6Scene.tscn")
+
+func changePanel() -> void:
+	if (counter != num+1):
+		counter += 1
