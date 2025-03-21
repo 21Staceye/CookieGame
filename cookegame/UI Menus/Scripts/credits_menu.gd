919c1bc90fb8de1f8b02ace328dbac969b930a6a
21Staceye <staceyerin5@gmail.com>
2025-03-21 11:16:39 -0400

    updated credits

--- cookegame/UI Menus/Scripts/credits_menu.gd
+++ cookegame/UI Menus/Scripts/credits_menu.gd
@@ -7,3 +7,10 @@ func _on_back_pressed() -> void:
 	$clickingButton.play()
 	await $clickingButton.finished
 	get_tree().change_scene_to_file("res://UI Menus/Scenes/MainMenu.tscn")
+
+
+func _on_next_pressed() -> void:
+	$clickingButton.play()
+	await $clickingButton.finished
+	$image.visible = false
+	$page2.visible = true
