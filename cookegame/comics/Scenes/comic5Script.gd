a7b3c4ccfbbb55c130c995d2bce9ebebe6a40bed
HannahKnisley <122892305+HannahKnisley@users.noreply.github.com>
2025-03-21 16:20:11 -0400

    comics fix and nerd lines

--- cookegame/comics/Scenes/comic5Script.gd
+++ cookegame/comics/Scenes/comic5Script.gd
@@ -9,11 +9,11 @@ var counter = 0
 
 func _process(delta: float) -> void:
 	panelList[counter].self_modulate.a += 0.5 * delta
-	if (voiceFinished && counter == num + 2):
+	if (voiceFinished && counter == num + 1 && Input.is_action_just_pressed("Interact")):
 		get_tree().change_scene_to_packed(sceneToChangeTo)
 
 func changePanel() -> void:
-	if (counter != num+2):
+	if (counter != num+1):
 		counter += 1
 
 
